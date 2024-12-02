9011e0d28504e1f8c96896e53ebc67551ce37dc1
angel.angelov711 <angel.krasimirov35@gmail.com>
2024-12-02 23:01:08 +0200

    node.js starter basics

---
+++ 4-names.js
@@ -0,0 +1,9 @@
+//local
+const secret = 'super secret'
+//share
+const john = 'John'
+const peter = 'Peter'
+
+console.log(module)
+
+module.exports = {john,peter}
---
+++ 7-mind-granade.js
@@ -0,0 +1,10 @@
+const num1 = 5;
+const num2 = 10;
+
+function addValues(){
+  console.log(`the sum is ${num1 + num2}`)
+}
+
+addValues()
+
+// you can import with require(./) directly and this will invokve the module as a function 
